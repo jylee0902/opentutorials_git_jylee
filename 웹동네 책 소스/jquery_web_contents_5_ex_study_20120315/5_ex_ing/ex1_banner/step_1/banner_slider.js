455896398395a50bbd5ac161ae4664ae292daab0
skyjong <wlsfh@lycos.co.kr>
2015-01-25 20:35:09 +0900

    study소스

---
+++ 웹동네 책 소스/jquery_web_contents_5_ex_study_20120315/5_ex_ing/ex1_banner/step_1/banner_slider.js
@@ -0,0 +1,37 @@
+/*
+
+
+힌트 :		
+	Step #9
+		-  autoPlay 기능 구현.
+		- banner slider에서 mouse enter, leave시 autoPlay 멈춤, 시작 처리.
+		
+	Step #8
+		- 이미 보여지고 있는 배너가 다시 보여지게 되는 경우 막기.
+		
+	Step #7
+		- banner navigation 에서 마우스가 오버되는 경우. 오버된 위치의 배너로 이동시키기.
+		
+	 Step #6
+	 	- banner navigation과 연동처리.
+	 	
+	 Step #5
+	 	- 리팩토링 2
+	 	
+	 Step #4
+	 	- 리팩토링1
+	 	
+	 Step #3
+	 	- 배너를 좌우로 이동시키기.
+	 	
+	 Step #2
+	 	- 동적으로 배너 컨텐츠 넓이 잡기.
+	 	
+	 Step #1
+	 	- 레이아웃 구조잡기.
+
+ */
+
+$(document).ready(function(){
+	// 여기서부터 소스가 시작한답니다.	
+});
